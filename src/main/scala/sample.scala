f229af1219ce19ebb730bd28f23ca098548fc843
Ramesh Naik <49408740+RameshRamavath@users.noreply.github.com>
2020-11-18 09:09:05 +0530

    Update sample.scala
    edited

--- src/main/scala/sample.scala
+++ src/main/scala/sample.scala
@@ -7,8 +7,6 @@ object sample {
   def main(args: Array[String]): Unit = {
        val spark =SparkSession.builder().master("local[2]").appName("test").getOrCreate()
 
-    //spark.read.text("/Users/313248/IdeaProjects/spark-test/src/main/resources/hello.txt").show()
-
     val sq = Seq((1,"ramesh"),(2,"mehesh"))
     val schema = StructType(Seq(StructField("SI NO",IntegerType, false),
       StructField("Name",StringType, false)))
